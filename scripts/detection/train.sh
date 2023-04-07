e1736444af12ef6d5b133fd1b154d38d6f8a9ef6
zhengsipeng <121569502@qq.com>
2023-04-07 17:48:23 +0800

    remove detection

--- scripts/detection/train.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
-        --master_port 47749 \
-        --nproc_per_node=1 main.py \
-        --accumulate_steps 1 \
-        --task detection \
-        --batch_size 1 --lr_backbone 1e-5 --lr 5e-5 --num_queries 200 \
-        --dataset_config configs/vidvrd_det.json
-        
