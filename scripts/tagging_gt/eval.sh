eb61f74fb15003864115d6dd342c8431d9fbd0f6
zhengsipeng <121569502@qq.com>
2023-04-07 17:47:48 +0800

    remove bak tagging_gt

--- scripts/tagging_gt/eval.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
-        --master_port 47751 \
-        --nproc_per_node=1 main.py \
-        --batch_size 1 \
-        --num_queries 200 \
-        --eval \
-        --dataset_config configs/vidvrd_tag_eval.json
-        
--- scripts/tagging_gt/train.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 CUBLAS_WORKSPACE_CONFIG=:16:8 python -m torch.distributed.launch \
-        --master_port 47749 \
-        --nproc_per_node=1 main.py \
-        --accumulate_steps 1 \
-        --batch_size 1 --lr_backbone 1e-5 --lr 5e-5 --num_queries 200 \
-        --dataset_config configs/vidvrd_tag_single_gpu.json
-        
